6ce04e949f4352e4db21d728a269b0234df52230
blazinzzetti <24198652+BlazinZzetti@users.noreply.github.com>
2023-03-11 20:46:39 -0800

    Remove ROM Settings
    This is now handled in the ROM no longer requiring Gecko Codes to manage.

--- SettingsDialog.Designer.cs
+++ SettingsDialog.Designer.cs
@@ -33,61 +33,43 @@ namespace ShadowSXLauncher
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsDialog));
             this.ControllerSettingsButton = new System.Windows.Forms.Button();
-            this.ModernUIControlCheckBox = new System.Windows.Forms.CheckBox();
             this.UiButtonDisplayComboBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.ShadowColorButton = new System.Windows.Forms.Button();
-            this.CutsceneSkipCheckBox = new System.Windows.Forms.CheckBox();
             this.GlossAdjustmentComboBox = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.RaceModeCheckBox = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
             this.RomLocationTextBox = new System.Windows.Forms.TextBox();
             this.SetRomLocationButton = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ControllerSettingsButton
             // 
             this.ControllerSettingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.ControllerSettingsButton.Location = new System.Drawing.Point(16, 278);
-            this.ControllerSettingsButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ControllerSettingsButton.Location = new System.Drawing.Point(12, 117);
             this.ControllerSettingsButton.Name = "ControllerSettingsButton";
-            this.ControllerSettingsButton.Size = new System.Drawing.Size(261, 63);
+            this.ControllerSettingsButton.Size = new System.Drawing.Size(196, 51);
             this.ControllerSettingsButton.TabIndex = 4;
             this.ControllerSettingsButton.Text = "Open Dolphin Interface";
             this.ControllerSettingsButton.UseVisualStyleBackColor = true;
             this.ControllerSettingsButton.Click += new System.EventHandler(this.ControllerSettingsButton_Click);
             // 
-            // ModernUIControlCheckBox
-            // 
-            this.ModernUIControlCheckBox.Location = new System.Drawing.Point(20, 142);
-            this.ModernUIControlCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.ModernUIControlCheckBox.Name = "ModernUIControlCheckBox";
-            this.ModernUIControlCheckBox.Size = new System.Drawing.Size(257, 30);
-            this.ModernUIControlCheckBox.TabIndex = 1;
-            this.ModernUIControlCheckBox.Text = "Use Modern UI Control (Xbox Style)";
-            this.ModernUIControlCheckBox.UseVisualStyleBackColor = true;
-            this.ModernUIControlCheckBox.CheckedChanged += new System.EventHandler(this.ModernUIControlCheckBox_CheckedChanged);
-            // 
             // UiButtonDisplayComboBox
             // 
             this.UiButtonDisplayComboBox.FormattingEnabled = true;
-            this.UiButtonDisplayComboBox.Location = new System.Drawing.Point(151, 203);
-            this.UiButtonDisplayComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.UiButtonDisplayComboBox.Location = new System.Drawing.Point(113, 58);
             this.UiButtonDisplayComboBox.Name = "UiButtonDisplayComboBox";
-            this.UiButtonDisplayComboBox.Size = new System.Drawing.Size(160, 24);
+            this.UiButtonDisplayComboBox.Size = new System.Drawing.Size(121, 21);
             this.UiButtonDisplayComboBox.TabIndex = 0;
             this.UiButtonDisplayComboBox.Text = "Steam Deck";
             this.UiButtonDisplayComboBox.SelectedValueChanged += new System.EventHandler(this.UiButtonDisplayComboBox_SelectedValueChanged);
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(16, 207);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 23);
+            this.label1.Size = new System.Drawing.Size(95, 19);
             this.label1.TabIndex = 3;
             this.label1.Text = "UI Buttons Display";
             // 
@@ -95,127 +77,84 @@ namespace ShadowSXLauncher
             // 
             this.ShadowColorButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.ShadowColorButton.Enabled = false;
-            this.ShadowColorButton.Location = new System.Drawing.Point(289, 278);
-            this.ShadowColorButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ShadowColorButton.Location = new System.Drawing.Point(217, 117);
             this.ShadowColorButton.Name = "ShadowColorButton";
-            this.ShadowColorButton.Size = new System.Drawing.Size(127, 63);
+            this.ShadowColorButton.Size = new System.Drawing.Size(95, 51);
             this.ShadowColorButton.TabIndex = 5;
             this.ShadowColorButton.Text = "Customize Shadow Color\r\n";
             this.ShadowColorButton.UseVisualStyleBackColor = true;
             this.ShadowColorButton.Click += new System.EventHandler(this.ShadowColorButton_Click);
             // 
-            // CutsceneSkipCheckBox
-            // 
-            this.CutsceneSkipCheckBox.Location = new System.Drawing.Point(20, 68);
-            this.CutsceneSkipCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.CutsceneSkipCheckBox.Name = "CutsceneSkipCheckBox";
-            this.CutsceneSkipCheckBox.Size = new System.Drawing.Size(257, 30);
-            this.CutsceneSkipCheckBox.TabIndex = 2;
-            this.CutsceneSkipCheckBox.Text = "Restore Original Cutscene Skipping";
-            this.CutsceneSkipCheckBox.UseVisualStyleBackColor = true;
-            this.CutsceneSkipCheckBox.CheckedChanged += new System.EventHandler(this.CutsceneSkipCheckBox_CheckedChanged);
-            // 
             // GlossAdjustmentComboBox
             // 
             this.GlossAdjustmentComboBox.FormattingEnabled = true;
-            this.GlossAdjustmentComboBox.Location = new System.Drawing.Point(219, 236);
-            this.GlossAdjustmentComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GlossAdjustmentComboBox.Location = new System.Drawing.Point(164, 85);
             this.GlossAdjustmentComboBox.Name = "GlossAdjustmentComboBox";
-            this.GlossAdjustmentComboBox.Size = new System.Drawing.Size(196, 24);
+            this.GlossAdjustmentComboBox.Size = new System.Drawing.Size(148, 21);
             this.GlossAdjustmentComboBox.TabIndex = 3;
             this.GlossAdjustmentComboBox.SelectedIndexChanged += new System.EventHandler(this.GlossAdjustmentComboBox_SelectedIndexChanged);
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(16, 240);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 88);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(191, 28);
+            this.label2.Size = new System.Drawing.Size(143, 23);
             this.label2.TabIndex = 7;
             this.label2.Text = "Glossy Effecty on Characters";
             // 
-            // RaceModeCheckBox
-            // 
-            this.RaceModeCheckBox.Location = new System.Drawing.Point(20, 105);
-            this.RaceModeCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.RaceModeCheckBox.Name = "RaceModeCheckBox";
-            this.RaceModeCheckBox.Size = new System.Drawing.Size(315, 30);
-            this.RaceModeCheckBox.TabIndex = 8;
-            this.RaceModeCheckBox.Text = "Show Total IGT for Story Runs (Race Mode)";
-            this.RaceModeCheckBox.UseVisualStyleBackColor = true;
-            this.RaceModeCheckBox.CheckedChanged += new System.EventHandler(this.RaceModeCheckBox_CheckedChanged);
-            // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(16, 11);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(127, 21);
+            this.label3.Size = new System.Drawing.Size(95, 17);
             this.label3.TabIndex = 9;
             this.label3.Text = "SX ROM Location";
             // 
             // RomLocationTextBox
             // 
-            this.RomLocationTextBox.Location = new System.Drawing.Point(151, 7);
-            this.RomLocationTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RomLocationTextBox.Location = new System.Drawing.Point(113, 6);
             this.RomLocationTextBox.Name = "RomLocationTextBox";
             this.RomLocationTextBox.ReadOnly = true;
-            this.RomLocationTextBox.Size = new System.Drawing.Size(223, 22);
+            this.RomLocationTextBox.Size = new System.Drawing.Size(168, 20);
             this.RomLocationTextBox.TabIndex = 10;
             // 
             // SetRomLocationButton
             // 
-            this.SetRomLocationButton.Location = new System.Drawing.Point(383, 5);
-            this.SetRomLocationButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SetRomLocationButton.Location = new System.Drawing.Point(287, 4);
             this.SetRomLocationButton.Name = "SetRomLocationButton";
-            this.SetRomLocationButton.Size = new System.Drawing.Size(33, 28);
+            this.SetRomLocationButton.Size = new System.Drawing.Size(25, 23);
             this.SetRomLocationButton.TabIndex = 11;
             this.SetRomLocationButton.Text = "...";
             this.SetRomLocationButton.UseVisualStyleBackColor = true;
             this.SetRomLocationButton.Click += new System.EventHandler(this.SetRomLocationButton_Click);
             // 
-            // label4
-            // 
-            this.label4.Location = new System.Drawing.Point(16, 43);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(133, 21);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "ROM Settings";
-            // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(16, 178);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(12, 38);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(133, 21);
+            this.label5.Size = new System.Drawing.Size(126, 17);
             this.label5.TabIndex = 13;
-            this.label5.Text = "Graphic Settings";
+            this.label5.Text = "Dolphin Graphic Settings";
             // 
             // SettingsDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(432, 356);
+            this.ClientSize = new System.Drawing.Size(324, 180);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.SetRomLocationButton);
             this.Controls.Add(this.RomLocationTextBox);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.RaceModeCheckBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.GlossAdjustmentComboBox);
-            this.Controls.Add(this.CutsceneSkipCheckBox);
             this.Controls.Add(this.ShadowColorButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.UiButtonDisplayComboBox);
-            this.Controls.Add(this.ModernUIControlCheckBox);
             this.Controls.Add(this.ControllerSettingsButton);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SettingsDialog";
@@ -223,10 +162,8 @@ namespace ShadowSXLauncher
             this.Text = "Settings";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
 
         private System.Windows.Forms.Button SetRomLocationButton;
@@ -235,20 +172,14 @@ namespace ShadowSXLauncher
 
         private System.Windows.Forms.Label label3;
 
-        private System.Windows.Forms.CheckBox RaceModeCheckBox;
-
         private System.Windows.Forms.ComboBox GlossAdjustmentComboBox;
         private System.Windows.Forms.Label label2;
-
-        private System.Windows.Forms.CheckBox CutsceneSkipCheckBox;
 
         private System.Windows.Forms.Button ControllerSettingsButton;
 
         private System.Windows.Forms.Label label1;
 
         private System.Windows.Forms.ComboBox UiButtonDisplayComboBox;
-
-        private System.Windows.Forms.CheckBox ModernUIControlCheckBox;
 
         private System.Windows.Forms.Button ShadowColorButton;
 
--- SettingsDialog.cs
+++ SettingsDialog.cs
@@ -19,34 +19,19 @@ namespace ShadowSXLauncher
             InitializeComponent();
             
             //Unregister events to initialize settings.
-            ModernUIControlCheckBox.CheckedChanged -= ModernUIControlCheckBox_CheckedChanged;
             UiButtonDisplayComboBox.SelectedValueChanged -= UiButtonDisplayComboBox_SelectedValueChanged;
-            CutsceneSkipCheckBox.CheckedChanged -= CutsceneSkipCheckBox_CheckedChanged;
             GlossAdjustmentComboBox.SelectedIndexChanged -= GlossAdjustmentComboBox_SelectedIndexChanged;
-            RaceModeCheckBox.CheckedChanged -= RaceModeCheckBox_CheckedChanged;
 
             UiButtonDisplayComboBox.DataSource = Configuration.UiButtonStyles.Values.ToList(); 
             GlossAdjustmentComboBox.DataSource = Configuration.GlossAdjustmentOptions.Values.ToList();
 
             RomLocationTextBox.Text = Configuration.Instance.RomLocation;
-            ModernUIControlCheckBox.Checked = Configuration.Instance.UseModernUiControl;
-            CutsceneSkipCheckBox.Checked = Configuration.Instance.DisableSkipCutscenes;
             GlossAdjustmentComboBox.SelectedIndex = Configuration.Instance.GlossAdjustmentIndex;
             UiButtonDisplayComboBox.SelectedIndex = Configuration.Instance.UiButtonDisplayIndex;
-            RaceModeCheckBox.Checked = Configuration.Instance.RaceMode;
-            
-            //Re-Register Events.
-            ModernUIControlCheckBox.CheckedChanged += ModernUIControlCheckBox_CheckedChanged;
-            UiButtonDisplayComboBox.SelectedValueChanged += UiButtonDisplayComboBox_SelectedValueChanged;
-            CutsceneSkipCheckBox.CheckedChanged += CutsceneSkipCheckBox_CheckedChanged;
-            GlossAdjustmentComboBox.SelectedIndexChanged += GlossAdjustmentComboBox_SelectedIndexChanged;
-            RaceModeCheckBox.CheckedChanged += RaceModeCheckBox_CheckedChanged;
-        }
 
-        private void ModernUIControlCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            Configuration.Instance.UseModernUiControl = ModernUIControlCheckBox.Checked;
-            Configuration.Instance.SaveSettings();
+            //Re-Register Events.
+            UiButtonDisplayComboBox.SelectedValueChanged += UiButtonDisplayComboBox_SelectedValueChanged;
+            GlossAdjustmentComboBox.SelectedIndexChanged += GlossAdjustmentComboBox_SelectedIndexChanged;
         }
 
         private void UiButtonDisplayComboBox_SelectedValueChanged(object sender, EventArgs e)
@@ -72,22 +57,10 @@ namespace ShadowSXLauncher
         {
             //colorDialog1.ShowDialog();
         }
-
-        private void CutsceneSkipCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            Configuration.Instance.DisableSkipCutscenes = CutsceneSkipCheckBox.Checked;
-            Configuration.Instance.SaveSettings();
-        }
-
+        
         private void GlossAdjustmentComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             Configuration.Instance.GlossAdjustmentIndex = GlossAdjustmentComboBox.SelectedIndex;
-            Configuration.Instance.SaveSettings();
-        }
-
-        private void RaceModeCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            Configuration.Instance.RaceMode = RaceModeCheckBox.Checked;
             Configuration.Instance.SaveSettings();
         }
 
